834c9f37dcb25225991c2f73b4815c758ac1b6b5
prafullnavkar-nxg <prafull.navkar@nxglabs.com>
2021-11-05 18:30:02 +0530

    changes in css file

--- src/StudentData.js
+++ src/StudentData.js
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import Parse from "parse/dist/parse.min.js";
 import "./App.css";
 import { Button, Input, List } from "antd";
-import {
-  CheckOutlined,
-  CloseOutlined,
-  PlusOutlined,
-  RedoOutlined,
-} from "@ant-design/icons";
+import logo from "./images/NXG_logo.png";
 
 const PARSE_APPLICATIION_ID = "QNRm8MgB7Qi1DjAD0Rw6qtjXLnTZ1fa7JGWBknS8";
 const PARSE_HOST_URL = "https://parseapi.back4app.com/";
@@ -62,7 +57,6 @@ export const StudentData = () => {
   const updateStudent = async function (studentId, done) {
     let Student = new Parse.Object("Student");
     Student.set("objectId", studentId);
-    
 
     Student.set("done", done);
     try {
@@ -96,35 +90,50 @@ export const StudentData = () => {
 
   return (
     <div>
+      <div className="header">
+        <img
+          className="header_logo"
+          src={logo}
+          alt="NXGlabs_logo"
+          style={{ width: "300px", height: "118px" }}
+        />
+      </div>
       <div className="container">
-        <h2 className="list_heading">Student Data</h2>
+      <h2 className="list_heading" >Student Data</h2>
+        <div className="flex_between">
+        
         {/* student read (refresh) button */}
-        <Button onClick={readStudents} icon={<RedoOutlined />}></Button>
-
+        <Button onClick={readStudents} className="refresh_btn">
+          Refresh
+        </Button>
+        </div>
+        <div className="new_student_wrapper flex_between">
         {/* Student create text input */}
-        <Input
-          value={newStudentTitle}
-          onChange={(event) => setNewStudentTitle(event.target.value)}
-          placeholder="Student Name"
-          size="large"
-        />
-        <Input
-          value={newStudentEmail}
-          onChange={(event) => setNewStudentEmail(event.target.value)}
-          placeholder="Student Email"
-          size="large"
-        />
-        <Input
-          value={newStudentAge}
-          onChange={(event) => setNewStudentAge(event.target.value)}
-          placeholder="Student Age"
-          size="large"
-        />
+        
+          <Input
+            value={newStudentTitle}
+            onChange={(event) => setNewStudentTitle(event.target.value)}
+            placeholder="Student Name"
+            size="large"
+          />
+          <Input
+            value={newStudentEmail}
+            onChange={(event) => setNewStudentEmail(event.target.value)}
+            placeholder="Student Email"
+            size="large"
+          />
+          <Input
+            value={newStudentAge}
+            onChange={(event) => setNewStudentAge(event.target.value)}
+            placeholder="Student Age"
+            size="large"
+          />  
         {/* Student create button */}
-        <Button onClick={createStudent} icon={<PlusOutlined />}>
+        <Button onClick={createStudent} className="add_btn">
           Add
         </Button>
 
+        </div>
         <div>
           {/* Student read results list */}
           {readResults !== null &&
@@ -141,28 +150,28 @@ export const StudentData = () => {
                           : "student_text"
                       }
                     >
-                      {item.get("title")} <br/>
-                      {item.get('Email')} <br/>
-                      {item.get('Age')} 
+                      Name: {item.get("title")} <span />
+                      Email: {item.get("Email")} <span />
+                      Age: {item.get("Age")}
                     </p>
-
+                    <div className="flex_row">
                     {/* Student update button */}
                     {item.get("done") !== true && (
                       <Button
                         onClick={() => updateStudent(item.id, true)}
-                        icon={
-                          <CheckOutlined className="student_button_icon_done" />
-                        }
-                      ></Button> 
+                        className="update_btn"
+                      >
+                        Activate
+                      </Button>
                     )}
                     {/* student delete button */}
                     <Button
                       onClick={() => deleteStudent(item.id)}
-                      icon={
-                        <CloseOutlined className="student_button_icon_remove" />
-                      }
-                    ></Button>
-                    <hr/>
+                      className="remove_btn"
+                    >
+                      Delete
+                    </Button>
+                    </div>  <hr/>
                   </List.Item>
                 )}
               />
