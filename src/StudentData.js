4685f7a2126dd0e4e1df5752f339c79885c4c439
prafullnavkar-nxg <prafull.navkar@nxglabs.com>
2021-11-15 12:59:59 +0530

    added table template

--- src/StudentData.js
+++ src/StudentData.js
@@ -1,8 +1,7 @@
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Parse from "parse";
-import "./App.css";
-
+// import "./App.css";
+import "./fresh-bootstrap-table.css";
 
 const PARSE_APPLICATION_ID = "QNRm8MgB7Qi1DjAD0Rw6qtjXLnTZ1fa7JGWBknS8";
 const PARSE_HOST_URL = "https://parseapi.back4app.com/";
@@ -11,25 +10,27 @@ const PARSE_JAVASCRIPT_KEY = "U8VhPQ0CNgBF16PmcvNvjZFJv33mXw7gjJfvHQJm";
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
-const COLOR_CODES = ["#808080","#F0FFFF"];
+// const COLOR_CODES = ["#808080","#F0FFFF"];
 
 export const StudentData = () => {
   const [readResults, setReadResults] = useState([]);
   const [newStudentTitle, setNewStudentTitle] = useState("");
   const [newStudentEmail, setNewStudentEmail] = useState("");
   const [newStudentAge, setNewStudentAge] = useState("");
+  // const [newStudentNumber, setNewStudentNumber]= useState("")
 
   const createStudent = async function () {
     const newStudentTitleValue = newStudentTitle;
     const newStudentEmailVaule = newStudentEmail;
     const newStudentAgeValue = Number(newStudentAge);
+    // const newStudentNumberValue = Number(newStudentNumber);
 
     let Student = new Parse.Object("Student");
     Student.set("title", newStudentTitleValue);
     Student.set("Email", newStudentEmailVaule);
     Student.set("Age", newStudentAgeValue);
     Student.set("done", false);
-
+    Student.set("SrNo", 1)
     try {
       await Student.save();
 
@@ -93,58 +94,75 @@ export const StudentData = () => {
       return false;
     }
   };
-  const getRowColor = (index) => {
-    return COLOR_CODES[index % 2];
-  };
+  // const getRowColor = (index) => {
+  //   return COLOR_CODES[index % 2];
+  // };
 
   return (
-    <div>
-      <div className="container">
-        <h2 className="list_heading">Student Data</h2>
-        <div className="flex_between"></div>
-        <div>
-          <form className="form-inline">
-            <input
-              value={newStudentTitle}
-              onChange={(event) => setNewStudentTitle(event.target.value)}
-              placeholder="Student Name"
-              size="large"
-            />
+    <>
+     
 
-            <input
-              value={newStudentEmail}
-              onChange={(event) => setNewStudentEmail(event.target.value)}
-              placeholder="Student Email"
-              size="large"
-            />
+      <div>
+        <div className="container">
+          <h2 className="list_heading">Student Data</h2>
+          <div className="flex_between"></div>
+          <div>
+            <form className="form-inline">
+              <input
+                value={newStudentTitle}
+                onChange={(event) => setNewStudentTitle(event.target.value)}
+                placeholder="Student Name"
+                size="large"
+              />
 
-            <input
-              value={newStudentAge}
-              onChange={(event) => setNewStudentAge(event.target.value)}
-              placeholder="Student Age"
-              size="large"
-            />
-            
-          </form>
-          <button onClick={createStudent} className="add_btn">
+              <input
+                value={newStudentEmail}
+                onChange={(event) => setNewStudentEmail(event.target.value)}
+                placeholder="Student Email"
+                size="large"
+              />
+
+              <input
+                value={newStudentAge}
+                onChange={(event) => setNewStudentAge(event.target.value)}
+                placeholder="Student Age"
+                size="large"
+              />
+            </form>
+            <button onClick={createStudent} className="add_btn">
               Add
             </button>
-        </div>
+          </div>
 
-        <div>
-          {/* Student read results list*/}
-          {readResults !== null &&
-            readResults !== undefined &&
-            readResults.length > 0 && ( 
-                  // <>
-                    <table>
-                        {readResults.map((item, index)=>{
-                          return (
-                          <tr style={{ backgroundColor: getRowColor(index) }}>
+          <div>
+            {/* Student read results list*/}
+            {readResults !== null &&
+              readResults !== undefined &&
+              readResults.length > 0 && (
+                <div className="fresh-table full-color-orange">
+                  <table id="fresh-table" className="table">
+                    <thead>
+                      <th data-field="SrNo">Sr.No.</th>
+                      <th data-field="title">Name</th>
+                      <th data-field="Email">Email</th>
+                      <th data-field="Age">Age</th>
+                      <th data-field="Action ">Action</th>
+                    </thead>
+                    {readResults.map((item, index) => {
+
+                      return (
+                        <tbody>
+                        <tr>
                           <td>
-                            <span>Name: {item.get("title")} </span>
-                            <span>Email: {item.get("Email")} </span>
-                            <span>Age: {item.get("Age")}</span>
+                           {item.get("SrNo")}</td>
+                          <td>
+                            {item.get("title")} 
+                          </td>
+                          <td>
+                            {item.get("Email")} 
+                          </td>
+                          <td>
+                            {item.get("Age")}
                           </td>
                           <td>
                             <button
@@ -175,16 +193,15 @@ export const StudentData = () => {
                             )}
                           </td>
                         </tr>
-                          )
-                        })}
-                         
-                    </table>
-                  // </>
-                )}
-          
+                        </tbody>
+                      );
+                    })}
+                  </table>
+                </div>
+              )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
-
